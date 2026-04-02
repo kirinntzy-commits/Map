561c350a7b1e383cb1539fd761c9f7b4fe4028a9
kirinntzy-commits <kirinntzy@gmail.com>
2026-04-02 16:50:46 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'ekt-tourism-v1';
+const CACHE_NAME = 'ekt-tourism-v2';
 const urlsToCache = [
     './',
     './index.html',
     './app.js',
-    './site.webmanifest',
+    './manifest.json',
     'https://unpkg.com/maplibre-gl@3.6.2/dist/maplibre-gl.css',
     'https://unpkg.com/maplibre-gl@3.6.2/dist/maplibre-gl.js'
 ];
